1cd180cd7fc5aaaf3342b9931cdabc46f1105cab
Aizirek11 <aizireksovetbekova3@gmail.com>
2026-02-02 19:02:37 +0600

    commit

--- okur_men/urls.py
+++ okur_men/urls.py
@@ -1,24 +1,14 @@
-"""
-URL configuration for okur_men project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# okur_men/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Главная страница прямо через функцию
+def home(request):
+    return HttpResponse("Hello, world! Django is running ✅")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('men.urls')),  # теперь сайт открывается прямо с /
+    path('', home),  # главная страница /
+    path('men/', include('men.urls')),  # остальные URL приложения men
 ]
+
